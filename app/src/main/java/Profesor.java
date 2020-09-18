b2d83549d7b8046341a83cfde7ab593bea78e7c5
Timana Lopez <jatimanalo@iemsantateresita.edu.co>
2020-09-17 19:01:15 -0500

    Entidades:profesor,Estudiante y Libro N°2

--- app/src/main/java/Profesor.java
+++ app/src/main/java/Profesor.java
@@ -1,14 +1,11 @@
-
-mport androidx.annotation.NonNull;
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-
-@Entity(tableName = "Estudainte")
-public class Estudiante {
-    public Estudiante (){}
+@Entity(tableName = "Profesor")
+public class Profesor {
+    public Profesor (){}
 
     @PrimaryKey
     @NonNull
@@ -17,24 +14,29 @@ public class Estudiante {
 
     @ColumnInfo(name = "nombre")
     private String mNombre;
-    @ColumnInfo (name = "grado")
-    private int mGrado;
+    @ColumnInfo (name = "asignatura")
+    private String mAsignatura;
     @ColumnInfo (name = "codigoinst")
-    private int mCodigoInst;
+    private String mCodigoInst;
     @ColumnInfo (name = "telefono")
-    private int mTelefono;
+    private String mTelefono;
     @ColumnInfo (name = "correoinst")
     private String mCorreoInst;
 
     public String getNombre(){return mNombre}
     public void  setNombre(String nombre){this.mNombre = nombre;}
-    public int getGrado(){return mGrado}
-    public void  setGrado(String grado){this.mGrado = grado; }
+    public String getAsignatura(){return mAsignatura}
+    public void  setAsignatura(String asignatura){this.mAsignatura = asignatura; }
     public int  getCodigoInst{return.mCodigoInst}
     public void  setCodigoInst(String codigoinst){this.mCodigoInst = codigoinst;}
     public int getTelefono{return.mTelefono}
     public void setTelefono(String telefono){this.mTelefono = telefono; }
     public String getmNombre{return.mCorreoInst}
     public void setmCorreoInst(String correoinst){this.mCorreoInst = correoinst;}
+
+
+
+
+
 
 }
