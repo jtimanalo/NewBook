4d06968486525cc6e35e6bfc1be20cf98f8a12a7
Esteban Mauricio Lopez <EsMur-1234>
2020-09-29 16:27:56 -0500

    activity_ajustes 29/09

--- app/src/main/java/libro.java
+++ app/src/main/java/libro.java
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "libro")
+@Entity(tableName = "Libro")
 public class libro {
     public libro(){
 
